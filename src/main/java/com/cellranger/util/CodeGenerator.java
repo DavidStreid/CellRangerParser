be1a2e360d4f150c0e638a82e6096bde81d3437c
David Streid <david.streid.nyc@gmail.com>
2019-08-29 16:38:01 -0400

    Integrating schema generator

--- src/main/java/com/cellranger/util/CodeGenerator.java
+++ src/main/java/com/cellranger/util/CodeGenerator.java
@@ -1,8 +1,14 @@
 package com.cellranger.util;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 import com.cellranger.util.model.CellRangerSummaryCountModel;
 import com.cellranger.util.model.CellRangerSummaryVDJModel;
@@ -15,6 +21,7 @@ import java.io.IOException;
 
 public class CodeGenerator {
     private static Map<String, FieldMapperModel> daoFileMap = new HashMap<String, FieldMapperModel>();
+    private static String SCHEMA_FILE = "/PATH/TO/cell-ranger-util/src/main/java/com/cellranger/util/dao/Schema.md";
 
     static {
         daoFileMap.put("CellRangerSummaryCountDao", new CellRangerSummaryCountModel());
@@ -22,10 +29,12 @@ public class CodeGenerator {
     }
 
     public static void main(String[] args){
+        MarkdownGenerator.removeMarkdownFile(SCHEMA_FILE);
         for (Map.Entry<String,FieldMapperModel> entry : daoFileMap.entrySet()) {
             // TODO - Generate the markdown for each DAO file
             try {
                 DaoCodeGenerator.generateDaoFile(entry.getKey(), entry.getValue());
+                MarkdownGenerator.writeMarkdown(SCHEMA_FILE, entry.getKey(), entry.getValue());
             } catch (IOException e) {
                 System.out.println("ERROR: Failed to generate code");
                 System.out.println(e);
